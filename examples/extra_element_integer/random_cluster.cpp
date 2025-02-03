5deaebaa4ef58f9abca40befd7631b336155cf7e
magnoxemo <walid.ahammed01@gmail.com>
2025-02-02 18:17:56 -0600

    implementing suggestions from gonuke.

--- examples/extra_element_integer/random_cluster.cpp
+++ examples/extra_element_integer/random_cluster.cpp
@@ -1,9 +1,3 @@
-//
-// Created by ebny_walid on 1/30/25.
-//
-//
-// Created by ebny_walid on 1/30/25.
-//
 #include "libmesh/libmesh.h"
 #include "libmesh/mesh.h"
 #include "libmesh/exodusII_io.h"
@@ -21,6 +15,93 @@
 
 using namespace libMesh;
 
+void CreateMesh(libMesh::Mesh & mesh, int nx, int ny){
+
+    libMesh::MeshTools::Generation::build_square(mesh,
+                                                 nx,
+                                                 ny,
+                                                 0.0, 10.0,
+                                                 0.0, 10.0,
+                                                 libMesh::QUAD4);
+}
+
+void CreateMesh(libMesh::Mesh & mesh, int nx, int ny, int nz ){
+    libMesh::MeshTools::Generation::build_cube(mesh,
+                                               nx,
+                                               ny,
+                                               nz,
+                                               0.0, 10.0,
+                                               0.0, 10.0,
+                                               0.0, 10.0);
+}
+
+void PopulateRandomIntegers(libMesh::Mesh & mesh, LinearImplicitSystem& system, const unsigned int &index ) {
+
+    DofMap& dof_map = system.get_dof_map();
+    for (const auto& elem : mesh.element_ptr_range()) {
+
+        std::vector<dof_id_type> dof_indices;
+        dof_map.dof_indices(elem, dof_indices);
+        int random_int = rand() %5;
+
+        system.solution->set(dof_indices[1], random_int);
+        elem->set_extra_integer(index, 5-random_int);
+
+    }
+}
+
+void AddVaribalesToSystem(LinearImplicitSystem& system, libMesh::EquationSystems & equation_systems){
+
+    system.add_variable("cluster_id", CONSTANT, MONOMIAL);
+    system.add_variable("random_field", CONSTANT, MONOMIAL);
+
+    equation_systems.init();
+}
+
+DofMap& CreateDegreesofFreedomMap(LinearImplicitSystem& system){
+    return  system.get_dof_map();
+}
+
+void FindCluster(libMesh::Mesh & mesh, libMesh::DofMap & dof_map, LinearImplicitSystem& system, const unsigned int index ) {
+
+    for (const auto &elem: mesh.element_ptr_range()) {
+
+        std::vector <dof_id_type> dof_indices;
+        dof_map.dof_indices(elem, dof_indices);
+
+        bool belong_to_a_cluster = false;
+
+        for (unsigned int side = 0; side < elem->n_sides(); ++side) {
+            const Elem *neighbor = elem->neighbor_ptr(side);
+
+            if (neighbor) {
+                if (elem->get_extra_integer(index) == neighbor->get_extra_integer(index)) {
+                    belong_to_a_cluster = true;
+                }
+            }
+        }
+        if (!belong_to_a_cluster) {
+            system.solution->set(dof_indices[0], 0);
+        } else if (belong_to_a_cluster) {
+            const unsigned int cluster_id = elem->get_extra_integer(index);
+            system.solution->set(dof_indices[0], cluster_id);
+        }
+    }
+}
+void CloseSystems(libMesh::Mesh & mesh, LinearImplicitSystem& system,libMesh::EquationSystems & equation_systems, bool print_system_info = true){
+
+    system.solution->close();
+    if (print_system_info){
+        mesh.print_info();
+        equation_systems.print_info();
+    }
+}
+
+void WriteEquationSystemOnTheMesh(libMesh::Mesh & mesh, libMesh::EquationSystems & equation_systems ){
+
+    ExodusII_IO(mesh).write_discontinuous_equation_systems("output_rand.e", equation_systems);
+}
+
 int main(int argc, char** argv) {
 
     LibMeshInit init(argc, argv);
@@ -30,94 +111,18 @@ int main(int argc, char** argv) {
     unsigned int ny = 40;
     srand(time(0));
 
-    libMesh::MeshTools::Generation::build_square(mesh,
-                                                 nx,
-                                                 ny,
-                                                 0.0, 10.0,
-                                                 0.0, 10.0,
-                                                 libMesh::QUAD4);
-
+    CreateMesh(mesh,nx,ny);
     const unsigned int index = mesh.add_elem_integer("cluster_id");
 
     EquationSystems equation_systems(mesh);
-
     LinearImplicitSystem& system = equation_systems.add_system<LinearImplicitSystem>("cluster");
-    system.add_variable("cluster_id", CONSTANT, MONOMIAL);
-    system.add_variable("random_field", CONSTANT, MONOMIAL);
+    DofMap& dof_map = CreateDegreesofFreedomMap(system);
+    AddVaribalesToSystem (system, equation_systems);
+    PopulateRandomIntegers (mesh,system,index);
+    FindCluster(mesh,dof_map,system,index);
+    CloseSystems(mesh,system,equation_systems);
 
-    equation_systems.init();
-
-    std::cout << "Number of DoFs: " << system.n_dofs() << std::endl;
-
-    DofMap& dof_map = system.get_dof_map();
-
-    for (const auto& elem : mesh.element_ptr_range()) {
-
-        std::vector<dof_id_type> dof_indices;
-        dof_map.dof_indices(elem, dof_indices);
-
-
-        /*I am gonna create a random number here from 1 to 4*/
-        int random_int = rand() %5;
-
-        for (auto dof_index : dof_indices) {
-            if (dof_index %2 !=0){
-                system.solution->set(dof_index, random_int);
-
-                if (random_int ==4){
-                    elem->set_extra_integer(index, 1);
-                } else if (random_int ==3){
-                    elem->set_extra_integer(index, 2);
-                }else if (random_int ==2){
-                    elem->set_extra_integer(index, 3);
-                }else if (random_int ==1){
-                    elem->set_extra_integer(index, 4);
-                }else if (random_int ==0){
-                    elem->set_extra_integer(index, 5);
-                }
-            }
-        }
-    }
-
-    for (const auto& elem : mesh.element_ptr_range()) {
-
-        std::vector<dof_id_type> dof_indices;
-        dof_map.dof_indices(elem, dof_indices);
-
-        for (auto dof_index : dof_indices) {
-            if (dof_index %2 ==0){
-
-                bool belong_to_a_cluster =false ;
-                for (unsigned int side = 0; side < elem->n_sides(); ++side){
-                    const Elem* neighbor = elem->neighbor_ptr(side);
-
-                    if (neighbor){
-                        if (elem->get_extra_integer(index)==neighbor->get_extra_integer(index)){
-                            belong_to_a_cluster = true ;
-                        }
-                    }
-                }
-                if (!belong_to_a_cluster){
-                    system.solution->set(dof_index,0);
-                }else if (belong_to_a_cluster){
-                    const unsigned int cluster_id =elem->get_extra_integer(index);
-                    system.solution->set(dof_index, cluster_id);
-                }
-
-            }
-        }
-    }
-
-    /* Now that I have some sort of random field here */
-
-
-
-    system.solution->close();
-
-    mesh.print_info();
-    equation_systems.print_info();
-
-    ExodusII_IO(mesh).write_discontinuous_equation_systems("output_rand.e", equation_systems);
+    WriteEquationSystemOnTheMesh(mesh,equation_systems);
 
     return 0;
 }
